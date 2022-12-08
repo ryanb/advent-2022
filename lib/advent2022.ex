2a90414aab3183a98e6137737842ca77645129c4
Ryan Bates <ryan@railscasts.com>
2022-12-08 02:10:19 -0800

    Initial commit

---
+++ lib/advent2022.ex
@@ -0,0 +1,18 @@
+defmodule Advent2022 do
+  @moduledoc """
+  Documentation for `Advent2022`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Advent2022.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/advent2022_test.exs
@@ -0,0 +1,8 @@
+defmodule Advent2022Test do
+  use ExUnit.Case
+  doctest Advent2022
+
+  test "greets the world" do
+    assert Advent2022.hello() == :world
+  end
+end
