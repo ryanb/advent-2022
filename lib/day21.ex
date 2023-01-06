8d2491803fa6695fd449f41ae6da9e80acfe1336
Ryan Bates <ryan@railscasts.com>
2023-01-06 00:49:33 -0800

    Add solution to day 21 part 2

--- lib/day21.ex
+++ lib/day21.ex
@@ -12,27 +12,110 @@ defmodule Day21.Part1 do
   end
 
   def parse_line(line, monkeys) do
-    [name, operation] = String.split(line, ": ")
+    [key, operation_str] = String.split(line, ": ")
 
-    if String.contains?(operation, " ") do
-      Map.put(monkeys, name, String.split(operation, " "))
+    if String.contains?(operation_str, " ") do
+      operation = operation_str |> String.split(" ") |> List.to_tuple()
+      Map.put(monkeys, key, operation)
     else
-      Map.put(monkeys, name, String.to_integer(operation))
+      Map.put(monkeys, key, String.to_integer(operation_str))
     end
   end
 
-  def evaluate(monkeys, name) do
-    case monkeys[name] do
-      [a, "+", b] -> evaluate(monkeys, a) + evaluate(monkeys, b)
-      [a, "-", b] -> evaluate(monkeys, a) - evaluate(monkeys, b)
-      [a, "*", b] -> evaluate(monkeys, a) * evaluate(monkeys, b)
-      [a, "/", b] -> round(evaluate(monkeys, a) / evaluate(monkeys, b))
-      _ -> monkeys[name]
+  def evaluate(monkeys, key) do
+    case monkeys[key] do
+      {a, "+", b} -> evaluate(monkeys, a) + evaluate(monkeys, b)
+      {a, "-", b} -> evaluate(monkeys, a) - evaluate(monkeys, b)
+      {a, "*", b} -> evaluate(monkeys, a) * evaluate(monkeys, b)
+      {a, "/", b} -> round(evaluate(monkeys, a) / evaluate(monkeys, b))
+      _ -> monkeys[key]
     end
   end
 end
 
 defmodule Day21.Part2 do
+  @human "humn"
+
+  def solve(input) do
+    input
+    |> Day21.Part1.parse_input()
+    |> human_value()
+  end
+
+  def human_value(monkeys) do
+    path_with_root = monkeys |> path_to_human("root")
+    value = non_human_value(monkeys, path_with_root)
+    path = tl(path_with_root)
+    human_value_for_path(path, monkeys, value)
+  end
+
+  def path_to_human(_monkeys, @human) do
+    [@human]
+  end
+
+  def path_to_human(monkeys, key) do
+    case monkeys[key] do
+      {a, _operator, b} ->
+        path = path_to_human(monkeys, a) || path_to_human(monkeys, b)
+
+        if path do
+          [key | path]
+        else
+          nil
+        end
+
+      _ ->
+        nil
+    end
+  end
+
+  def human_value_for_path([@human], _monkeys, value) do
+    value
+  end
+
+  def human_value_for_path(path, monkeys, value) do
+    operation = flatten_operation(monkeys, path)
+    next_value = reverse_operation(operation, value)
+    human_value_for_path(tl(path), monkeys, next_value)
+  end
+
+  def non_human_value(monkeys, path) do
+    case flatten_operation(monkeys, path) do
+      {:humn, _operator, value} -> value
+      {value, _operator, :humn} -> value
+      _ -> throw("Unable to find value for path #{inspect(path)}")
+    end
+  end
+
+  def flatten_operation(monkeys, path) do
+    operation = monkeys[hd(path)]
+    human_key = path |> tl() |> hd()
+
+    case operation do
+      {non_human_key, operator, ^human_key} ->
+        {Day21.Part1.evaluate(monkeys, non_human_key), operator, :humn}
+
+      {^human_key, operator, non_human_key} ->
+        {:humn, operator, Day21.Part1.evaluate(monkeys, non_human_key)}
+
+      _ ->
+        throw("Operation #{inspect(operation)} does not have #{human_key}")
+    end
+  end
+
+  def reverse_operation(operation, value) do
+    case operation do
+      {:humn, "+", a} -> value - a
+      {a, "+", :humn} -> value - a
+      {:humn, "-", a} -> value + a
+      {a, "-", :humn} -> a - value
+      {:humn, "*", a} -> round(value / a)
+      {a, "*", :humn} -> round(value / a)
+      {:humn, "/", a} -> value * a
+      {a, "/", :humn} -> round(a / value)
+      _ -> throw("Unexpected operation: #{inspect(operation)}")
+    end
+  end
 end
 
 defmodule Mix.Tasks.Day21 do
@@ -43,8 +126,8 @@ defmodule Mix.Tasks.Day21 do
 
     IO.puts("--- Part 1 ---")
     IO.puts(Day21.Part1.solve(input))
-    # IO.puts("")
-    # IO.puts("--- Part 2 ---")
-    # IO.puts(Day21.Part2.solve(input))
+    IO.puts("")
+    IO.puts("--- Part 2 ---")
+    IO.puts(Day21.Part2.solve(input))
   end
 end
--- test/day21_test.exs
+++ test/day21_test.exs
@@ -22,4 +22,40 @@ defmodule Day21Test do
   test "solves example input for part 1" do
     assert Day21.Part1.solve(@example_input) == 152
   end
+
+  test "solves example input for part 2" do
+    assert Day21.Part2.solve(@example_input) == 301
+  end
+
+  describe ".path_to_human" do
+    test "returns list of monkeys to human" do
+      monkeys = %{
+        "root" => {"a", "+", "b"},
+        "a" => 1,
+        "b" => {"c", "+", "d"},
+        "c" => {"e", "+", "humn"},
+        "d" => 2,
+        "e" => 3,
+        "humn" => 4
+      }
+
+      assert Day21.Part2.path_to_human(monkeys, "root") == ["root", "b", "c", "humn"]
+    end
+  end
+
+  describe ".reverse_operation" do
+    test "reverses math operation" do
+      assert Day21.Part2.reverse_operation({:humn, "+", 3}, 5) == 2
+      assert Day21.Part2.reverse_operation({3, "+", :humn}, 5) == 2
+
+      assert Day21.Part2.reverse_operation({:humn, "-", 3}, 5) == 8
+      assert Day21.Part2.reverse_operation({8, "-", :humn}, 5) == 3
+
+      assert Day21.Part2.reverse_operation({:humn, "*", 3}, 6) == 2
+      assert Day21.Part2.reverse_operation({3, "*", :humn}, 6) == 2
+
+      assert Day21.Part2.reverse_operation({:humn, "/", 3}, 2) == 6
+      assert Day21.Part2.reverse_operation({6, "/", :humn}, 2) == 3
+    end
+  end
 end
