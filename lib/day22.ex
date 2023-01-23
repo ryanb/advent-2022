4e48b8461c218fd7506ad7e7986f10111c02a1d6
Ryan Bates <ryan@railscasts.com>
2023-01-23 07:49:23 -0800

    Add solution to day 22 part 2

--- lib/day22.ex
+++ lib/day22.ex
@@ -150,6 +150,260 @@ defmodule Day22.Part1 do
 end
 
 defmodule Day22.Part2 do
+  @directions [:north, :east, :south, :west]
+
+  def solve(input, scale) do
+    input
+    |> parse_input(scale)
+    |> follow_commands()
+    |> Day22.Part1.result()
+  end
+
+  def parse_input(input, scale) do
+    [map_str, commands_str] = String.split(input, "\n\n", trim: true)
+    map = parse_map(map_str, scale)
+    commands = Day22.Part1.parse_commands(commands_str)
+    {start_x, _} = :binary.match(map_str, ".")
+
+    map
+    |> Map.merge(%{
+      faces: Day22.Part2.FacesBuilder.build_faces(map.minimap),
+      commands: commands,
+      position: {start_x, 0},
+      direction: :east
+    })
+  end
+
+  def parse_map(map_str, scale) do
+    map = %{minimap: MapSet.new(), walls: MapSet.new(), scale: scale}
+
+    map_str
+    |> String.split("\n", trim: true)
+    |> Enum.with_index()
+    |> Enum.reduce(map, &parse_map_line/2)
+  end
+
+  def parse_map_line({line, y}, map) do
+    line
+    |> String.graphemes()
+    |> Enum.with_index()
+    |> Enum.reduce(map, &parse_map_tile(&1, y, &2))
+  end
+
+  def parse_map_tile({tile, x}, y, map) do
+    minimap = MapSet.put(map.minimap, minimap_position({x, y}, map.scale))
+
+    case tile do
+      "#" -> %{map | minimap: minimap, walls: MapSet.put(map.walls, {x, y})}
+      "." -> %{map | minimap: minimap}
+      _ -> map
+    end
+  end
+
+  def follow_commands(%{commands: []} = state) do
+    state
+  end
+
+  def follow_commands(state) do
+    next_state = follow_command(state)
+    follow_commands(%{next_state | commands: tl(state.commands)})
+  end
+
+  def follow_command(state) do
+    command = hd(state.commands)
+
+    case command do
+      "R" -> Day22.Part1.turn(state, 1)
+      "L" -> Day22.Part1.turn(state, -1)
+      _ -> walk(state, String.to_integer(command))
+    end
+  end
+
+  def walk(state, 0) do
+    state
+  end
+
+  def walk(state, amount) do
+    {position, direction} = walk_position_and_direction(state)
+
+    if MapSet.member?(state.walls, position) do
+      state
+    else
+      walk(%{state | position: position, direction: direction}, amount - 1)
+    end
+  end
+
+  def walk_position_and_direction(state) do
+    position = Day22.Part1.facing_position(state)
+
+    if MapSet.member?(state.minimap, minimap_position(position, state.scale)) do
+      {position, state.direction}
+    else
+      walk_with_wrap(state)
+    end
+  end
+
+  def minimap_position({x, y}, scale) do
+    {floor(x / scale), floor(y / scale)}
+  end
+
+  def direction_index(direction) do
+    Enum.find_index(@directions, fn d -> d == direction end)
+  end
+
+  def walk_with_wrap(state) do
+    mini_position = minimap_position(state.position, state.scale)
+
+    next_mini_position = state.faces[mini_position] |> Enum.at(direction_index(state.direction))
+
+    from_direction_index =
+      state.faces[next_mini_position] |> Enum.find_index(fn p -> p == mini_position end)
+
+    next_direction = Enum.at(@directions, rem(from_direction_index + 2, 4))
+
+    offset = position_to_offset(state)
+
+    {rx, ry} =
+      offset_to_position(%{offset: offset, scale: state.scale, direction: next_direction})
+
+    {mx, my} = next_mini_position
+    next_position = {mx * state.scale + rx, my * state.scale + ry}
+
+    {next_position, next_direction}
+  end
+
+  def position_to_offset(%{position: {x, y}, scale: scale, direction: direction}) do
+    rx = rem(x, scale)
+    ry = rem(y, scale)
+
+    case direction do
+      :north -> rx
+      :east -> ry
+      :south -> abs(scale - 1 - rx)
+      :west -> abs(scale - 1 - ry)
+    end
+  end
+
+  def offset_to_position(%{offset: offset, scale: scale, direction: direction}) do
+    case direction do
+      :north -> {offset, scale - 1}
+      :east -> {0, offset}
+      :south -> {abs(scale - 1 - offset), 0}
+      :west -> {scale - 1, abs(scale - 1 - offset)}
+    end
+  end
+
+  defmodule FacesBuilder do
+    # The template is a map of faces to determine the neighbors
+    # for the following pattern.
+    #
+    #  1
+    # 025
+    #  3
+    #  4
+    #
+    @template %{
+      0 => [1, 2, 3, 4],
+      1 => [4, 5, 2, 0],
+      2 => [1, 5, 3, 0],
+      3 => [2, 5, 4, 0],
+      4 => [3, 5, 1, 0],
+      5 => [1, 4, 3, 2]
+    }
+
+    # This returns a map of positions to neighboring positions.
+    # It is built by mapping the template to a list of intermediate
+    # faces and mapping that to the final faces.
+    # minimap is a set of {x, y} positions that occupy tiles.
+    def build_faces(minimap) do
+      intermediate_faces = build_intermediate_faces(minimap)
+
+      intermediate_faces
+      |> Map.values()
+      |> Enum.reduce(%{}, &build_face(&1, &2, intermediate_faces))
+    end
+
+    def build_face(intermediate_face, faces, intermediate_faces) do
+      neighbors =
+        Enum.map(intermediate_face.neighbors, fn index ->
+          intermediate_faces[index].position
+        end)
+
+      Map.put(faces, intermediate_face.position, neighbors)
+    end
+
+    # Intermediate faces is a map of indexes based on the template. The value of each is a map
+    # with the following attributes.
+    #
+    #   position: the {x, y} position on the minimap.
+    #   neighbors: A list of neighbor indexes starting from the north side and going clockwise.
+    #
+    def build_intermediate_faces(minimap) do
+      position = MapSet.to_list(minimap) |> hd()
+
+      expand_intermediate_faces(
+        intermediate_faces: %{},
+        minimap: minimap,
+        index: 0,
+        rotation: 0,
+        position: position,
+        from_position: nil
+      )
+    end
+
+    def rotate_neighbors(neighbors, amount) do
+      {last, first} = Enum.split(neighbors, rem(amount, length(neighbors)))
+      first ++ last
+    end
+
+    def expand_intermediate_faces(
+          intermediate_faces: intermediate_faces,
+          minimap: minimap,
+          index: index,
+          rotation: rotation,
+          position: position,
+          from_position: from_position
+        ) do
+      neighbors = @template[index] |> rotate_neighbors(rotation)
+
+      expanded_intermediate_faces =
+        Map.put(intermediate_faces, index, %{position: position, neighbors: neighbors})
+
+      [{0, -1}, {1, 0}, {0, 1}, {-1, 0}]
+      |> Enum.with_index()
+      |> Enum.reduce(expanded_intermediate_faces, fn {{rx, ry}, direction},
+                                                     next_intermediate_faces ->
+        {x, y} = position
+        direction_position = {x + rx, y + ry}
+
+        if direction_position == from_position || !Enum.member?(minimap, direction_position) do
+          next_intermediate_faces
+        else
+          next_index = Enum.at(neighbors, direction)
+
+          rotation =
+            calculate_rotation(index: index, direction: direction, next_index: next_index)
+
+          expand_intermediate_faces(
+            intermediate_faces: next_intermediate_faces,
+            minimap: minimap,
+            index: next_index,
+            rotation: rotation,
+            position: direction_position,
+            from_position: position
+          )
+        end
+      end)
+    end
+
+    # The rotation is how the real face should be rotated in relation to the template face.
+    # It is calculated by getting the difference of the directions we're coming from.
+    # We add 2 to get the opposite direction so we are comparing the same side
+    def calculate_rotation(index: index, direction: direction, next_index: next_index) do
+      from_direction = Enum.find_index(@template[next_index], fn value -> value == index end)
+      abs(from_direction + 2 - direction)
+    end
+  end
 end
 
 defmodule Mix.Tasks.Day22 do
@@ -160,8 +414,8 @@ defmodule Mix.Tasks.Day22 do
 
     IO.puts("--- Part 1 ---")
     IO.puts(Day22.Part1.solve(input))
-    # IO.puts("")
-    # IO.puts("--- Part 2 ---")
-    # IO.puts(Day22.Part2.solve(input))
+    IO.puts("")
+    IO.puts("--- Part 2 ---")
+    IO.puts(Day22.Part2.solve(input, 50))
   end
 end
--- test/day22_test.exs
+++ test/day22_test.exs
@@ -45,4 +45,101 @@ defmodule Day22Test do
       assert Day22.Part1.parse_commands(input) == ["12", "R", "3", "L", "L", "45"]
     end
   end
+
+  # Part 2
+
+  describe "Part2.FacesBuilder.build_faces" do
+    test "returns faces given minimap of example input" do
+      input = """
+        .
+      ...
+        ..
+      """
+
+      minimap = Day22.Part1.parse_map(input).tiles
+
+      expected = %{
+        {0, 1} => [{2, 0}, {1, 1}, {2, 2}, {3, 2}],
+        {1, 1} => [{2, 0}, {2, 1}, {2, 2}, {0, 1}],
+        {2, 0} => [{0, 1}, {3, 2}, {2, 1}, {1, 1}],
+        {2, 1} => [{2, 0}, {3, 2}, {2, 2}, {1, 1}],
+        {2, 2} => [{2, 1}, {3, 2}, {0, 1}, {1, 1}],
+        {3, 2} => [{2, 1}, {2, 0}, {0, 1}, {2, 2}]
+      }
+
+      assert Day22.Part2.FacesBuilder.build_faces(minimap) == expected
+    end
+
+    test "returns faces given minimap of problem input" do
+      input = """
+       ..
+       .
+      ..
+      .
+      """
+
+      minimap = Day22.Part1.parse_map(input).tiles
+
+      expected = %{
+        {1, 1} => [{1, 0}, {2, 0}, {1, 2}, {0, 2}],
+        {2, 0} => [{0, 3}, {1, 2}, {1, 1}, {1, 0}],
+        {0, 2} => [{1, 1}, {1, 2}, {0, 3}, {1, 0}],
+        {0, 3} => [{0, 2}, {1, 2}, {2, 0}, {1, 0}],
+        {1, 0} => [{0, 3}, {2, 0}, {1, 1}, {0, 2}],
+        {1, 2} => [{1, 1}, {2, 0}, {0, 3}, {0, 2}]
+      }
+
+      assert Day22.Part2.FacesBuilder.build_faces(minimap) == expected
+    end
+  end
+
+  describe "Part2.FacesBuilder.rotate_neighbors" do
+    test "returns array rotated by the amount" do
+      assert Day22.Part2.FacesBuilder.rotate_neighbors([1, 2, 3], 1) == [2, 3, 1]
+      assert Day22.Part2.FacesBuilder.rotate_neighbors([1, 2, 3], 4) == [2, 3, 1]
+      assert Day22.Part2.FacesBuilder.rotate_neighbors([1, 2, 3], -1) == [3, 1, 2]
+      assert Day22.Part2.FacesBuilder.rotate_neighbors([1, 2, 3], -4) == [3, 1, 2]
+    end
+  end
+
+  describe "Part2.parse_map" do
+    test "adds walls and tiles to map" do
+      input = """
+        .#
+        ..
+      ....
+      .#..
+      """
+
+      expected = %{
+        minimap: MapSet.new([{1, 0}, {0, 1}, {1, 1}]),
+        walls: MapSet.new([{3, 0}, {1, 3}]),
+        scale: 2
+      }
+
+      assert Day22.Part2.parse_map(input, 2) == expected
+    end
+  end
+
+  describe "Part2.position_to_offset" do
+    test "returns offset for each direction" do
+      assert Day22.Part2.position_to_offset(%{position: {5, 0}, direction: :north, scale: 4}) == 1
+      assert Day22.Part2.position_to_offset(%{position: {0, 5}, direction: :east, scale: 4}) == 1
+      assert Day22.Part2.position_to_offset(%{position: {5, 0}, direction: :south, scale: 4}) == 2
+      assert Day22.Part2.position_to_offset(%{position: {0, 5}, direction: :west, scale: 4}) == 2
+    end
+  end
+
+  describe "Part2.offset_to_position" do
+    test "returns position for each direction" do
+      assert Day22.Part2.offset_to_position(%{offset: 1, direction: :north, scale: 4}) == {1, 3}
+      assert Day22.Part2.offset_to_position(%{offset: 1, direction: :east, scale: 4}) == {0, 1}
+      assert Day22.Part2.offset_to_position(%{offset: 1, direction: :south, scale: 4}) == {2, 0}
+      assert Day22.Part2.offset_to_position(%{offset: 1, direction: :west, scale: 4}) == {3, 2}
+    end
+  end
+
+  test "solves example input for part 2" do
+    assert Day22.Part2.solve(@example_input, 4) == 5031
+  end
 end
